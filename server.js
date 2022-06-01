6f51ad620104bae5b42912b38275f7a956f0257b
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-01 01:51:23 -0300

    implemented feature to show dolar price when dolar is setted as a message

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var axios = require('axios');
 
 var Message = mongoose.model('Message', { name: String, message: String })
 
@@ -25,8 +26,26 @@ app.post('/messages', async (req, res) => {
     message.save((err) => {
         if (err) sendStatus(500);
         io.emit('message', req.body);
-        res.sendStatus(200);
+        // res.sendStatus(200);
     })
+
+    if (req.body.message === 'dolar' || 'dólar') {
+        console.log('CAIU AQUI')
+        let response = await axios('http://economia.awesomeapi.com.br/json/last/USD-BRL');
+        let dolarPrice = parseFloat(response.data.USDBRL.high).toFixed(2);
+        const body = {
+            name: 'Dolar price bot',
+            message: `Dolar price in BRL: ${dolarPrice}`
+        }
+        message = new Message(body);
+        message.save((err) => {
+            if (err) sendStatus(500);
+            io.emit('message', body);
+
+        })
+    }
+
+    res.sendStatus(200);
 
 })
 
