4fc8f895e3d767e1b7378f0dc5b7fb31ca8e4a46
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-01 01:12:45 -0300

    fixed socket io error

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose');
-var http = require('http').Server(app);
-var axios = require('axios')
-// var io = require('socket.io')(http);
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var Message = mongoose.model('Message', { name: String, message: String })
 
@@ -25,20 +24,20 @@ app.post('/messages', async (req, res) => {
 
     message.save((err) => {
         if (err) sendStatus(500);
-        // io.emit('message', req.body);
+        io.emit('message', req.body);
         res.sendStatus(200);
     })
 
 })
 
-// io.on('connection', () => {
-//     console.log('a user is connected')
-// })
+io.on('connection', () => {
+    console.log('a user is connected')
+})
 
 mongoose.connect(process.env.MONGO_DB_URL, (err) => {
     console.log('mongodb connected', err);
 })
 
-var server = app.listen(3000, () => {
+var server = http.listen(3000, () => {
     console.log('server is running on port', server.address().port);
 });
