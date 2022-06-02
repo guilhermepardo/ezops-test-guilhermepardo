7feed929184aefc8b6dee4b94a1a754ad42802b0
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-02 19:15:11 -0300

    maybe this will go

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,10 @@ sudo chmod -R 777 /home/ec2-user/ezops-test-guilhermepardo
 
 cd /home/ec2-user/ezops-test-guilhermepardo
 
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
 npm install
 
 npm start
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install v16.15.0
+
 DIR="/home/ec2-user/repos/ezops-test-guilhermepardo"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
