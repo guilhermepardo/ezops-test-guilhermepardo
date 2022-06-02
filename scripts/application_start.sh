2a3d79b0e7b45a037fec96b25c3f7228f74d5acf
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-02 17:55:20 -0300

    changing directory in application_start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,8 +2,8 @@
 
 sudo chmod -R 777 /home/ec2-user/ezops-test-guilhermepardo
 
-cd /home/ec2-user/repos/ezops-test-guilhermepardo
+cd /home/ec2-user/ezops-test-guilhermepardo
 
 npm install
 
-npm start > server.out.log 2> server.err.log < /dev/null &
+npm start
