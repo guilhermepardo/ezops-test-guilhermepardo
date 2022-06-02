529d0a14c7f2019199c7cf64649370d659c39811
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-02 17:49:40 -0300

    changing directory in script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-sudo chmod -R 777 /home/ec2-user/repos/ezops-test-guilhermepardo
+sudo chmod -R 777 /home/ec2-user/ezops-test-guilhermepardo
 
 cd /home/ec2-user/repos/ezops-test-guilhermepardo
-
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 npm install
 
