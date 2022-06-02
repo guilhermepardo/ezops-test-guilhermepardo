a091a1129f9ffe7789a47cdbee026bb792171001
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-02 17:40:07 -0300

    removing install nvm from script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-
 DIR="/home/ec2-user/repos/ezops-test-guilhermepardo"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
