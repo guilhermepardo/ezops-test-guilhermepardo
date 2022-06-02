547cdc5fa07087f9511288904653a4ab2e2ed09a
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-02 14:20:57 -0300

    alterations in automation scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 sudo chmod -R 777 /home/ec2-user/repos/ezops-test-guilhermepardo
 
 cd /home/ec2-user/repos/ezops-test-guilhermepardo
+
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 npm install
 
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 echo "Stopping any existing node servers"
 pkill node
