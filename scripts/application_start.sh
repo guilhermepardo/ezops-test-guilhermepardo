9e2d811576ca87251052d39f65046dfd1462c1a8
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-03 17:46:06 -0300

    rollback commit

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,4 @@ cd /home/ec2-user/ezops-test-guilhermepardo
 
 docker build -t ezops-test-container .
 
-cd /home/ec2-user
-
-./docker_run.sh
+docker run -d -p 80:3000 -e MONGO_DB_URL="mongodb+srv://admin:2r3g6gxsx459Ie93@cluster0.xdpxo.mongodb.net/db?retryWrites=true&w=majority" ezops-test-container
