2a91a5af77f6334e1a8cdc6c4653310cdc6173db
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-02 21:06:28 -0300

    so muuuuuuuuuuch changes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+sudo chmod -R 777 /home/ec2-user/express-app
 
-sudo chmod -R 777 /home/ec2-user/ezops-test-guilhermepardo
-
+#navigate into our working directory where we have all our github files
 cd /home/ec2-user/ezops-test-guilhermepardo
 
+#add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+#install node modules
 npm install
 
-npm start
+#start our node app in the background
+node server.js > server.out.log 2> server.err.log < /dev/null & 
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+#download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install v16.15.0
+nvm install node
 
-DIR="/home/ec2-user/repos/ezops-test-guilhermepardo"
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/ezops-test-guilhermepardo
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
